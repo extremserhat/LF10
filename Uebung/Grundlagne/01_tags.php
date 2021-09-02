fdbe7e19e4de2aae85d706a2f58eccabdad06ad5
extremserhat <Serhat.Leylek@gfn.education>
2021-09-02 19:41:44 +0200

    neu

---
+++ Uebung/Grundlagne/01_tags.php
@@ -0,0 +1,13 @@
+<?php
+echo 'Hallo Welt';
+
+echo 'text bereich';?>
+Kein Zeilen Umbruch
+<?= "aber jetzt zeilen umbruch"?>
+<br>
+<?php
+    echo 'Dies ist ein Test'; ?>
+<br>
+<?php echo 'Dies ist ein Test' ?>
+<br>
+<?php echo 'Wir ließen den letzten schließenden Tag weg';
